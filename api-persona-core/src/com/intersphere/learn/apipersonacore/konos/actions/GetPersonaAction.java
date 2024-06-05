0478b1afb2f660dea7b40952b81846bc241c8121
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2024-06-05 10:18:53 -0600

    Return list of Persons

--- api-persona-core/src/com/intersphere/learn/apipersonacore/konos/actions/GetPersonaAction.java
+++ api-persona-core/src/com/intersphere/learn/apipersonacore/konos/actions/GetPersonaAction.java
@@ -1,6 +1,7 @@
 package com.intersphere.learn.apipersonacore.konos.actions;
 
 import com.intersphere.learn.apipersonacore.konos.ApiPersonaCoreBox;
+import com.intersphere.learn.apipersonacore.konos.schemas.PersonModel;
 import io.intino.alexandria.exceptions.*;
 import java.time.*;
 import java.util.*;
@@ -10,8 +11,19 @@ public class GetPersonaAction implements io.intino.alexandria.rest.RequestErrorH
 	public ApiPersonaCoreBox box;
 	public io.intino.alexandria.http.spark.SparkContext context;
 
-	public com.intersphere.learn.apipersonacore.konos.schemas.PersonModel execute() throws BadRequest, NotFound {
-		return "Soy Api Persona";
+	public List<PersonModel> execute() throws BadRequest, NotFound {
+		List<PersonModel> list = new ArrayList();
+		PersonModel person = new PersonModel();
+		person.name("Miguel");
+		person.lastName("Mexicano");
+		person.age(32);
+		PersonModel person2 = new PersonModel();
+		person2.name("Juan");
+		person2.lastName("Perez");
+		person2.age(18);
+		list.add(person);
+		list.add(person2);
+		return list;
 	}
 
 	public void onMalformedRequest(Throwable e) throws AlexandriaException {
